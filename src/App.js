f874289e69a32234514042efab2b59291823b5f9
Emna Gafsi <egafsi@evolutive-business.com>
2020-04-05 16:54:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>my app</h1>
+      <form>
+        <label className="d-block" >
+          name:
+        </label>
+        <input type="text" className="d-block" />
+        <label className="d-block" >
+          last name:
+        </label>
+        <input type="text" className="d-block" />
+        <label className="d-block" >
+         email:
+        </label>
+        <input type="email" className="d-block" />
+    
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
     </div>
   );
 }
